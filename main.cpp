922c2e0c633622301c8e1d2acbda9c9dfcb01040
Stéphane Kmieciak <stephane.kmieciak@douane.finances.gouv.fr>
2024-12-13 16:17:21 +0100

    Create basic app

--- main.cpp
+++ main.cpp
@@ -1,7 +1,69 @@
 #include <iostream>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
 
 int main()
 {
-    std::cout << "Hello World!";
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+    {
+        std::cout << "Failed to init GLFW";
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+    if (!window)
+    {
+        // Window or OpenGL context creation failed
+    }
+
+    glfwMakeContextCurrent(window);
+    int res = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    if(res == 0)
+    {
+        std::cout << "Failed to load GLAD";
+    }
+
+    // get version info
+    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+    const GLubyte* version = glGetString(GL_VERSION); // version as a string
+    printf("Renderer: %s\n", renderer);
+    printf("OpenGL version supported %s\n", version);
+
+    glfwSetKeyCallback(window, key_callback);
+    
+    while (!glfwWindowShouldClose(window))
+    {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+
+        glfwPollEvents();
+
+        glfwSwapBuffers(window);
+        glfwSwapInterval(1);
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    
     return 0;
 }
